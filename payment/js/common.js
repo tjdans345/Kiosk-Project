608fd58fb50925c4df1b19141f479df1bf871327
tjdans34568 <tjdans34568@gmail.com>
2022-04-15 00:20:01 +0900

    ..

---
+++ payment/js/common.js
@@ -0,0 +1,24 @@
+const timeOut = () => {
+  let time = 60;
+  let min = "";
+  let sec = "";
+
+  let x = setInterval(function () {
+    // parserInt() : 정수 반환
+    min = parseInt(time / 60); // 몫 계산
+    sec = time % 60; // 나머지 계산
+
+    document.getElementById("time_display").innerHTML = `${min} 분 ${sec} 초`;
+    time--;
+
+    if (time < 0) {
+      clearInterval(x);
+      alert("시간 초과 입니다.");
+      history.back();
+    }
+  }, 1000);
+};
+
+window.onload = function () {
+  timeOut();
+};
